a1d887b3b9793ae46aa12e769fae3dbe310bf94f
xun6000 <xun6000@gmail.com>
2017-02-06 12:04:07 -0600

    Add files via upload

--- hw2draft/knn/k_nearest_neighbor.py
+++ hw2draft/knn/k_nearest_neighbor.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 from collections import Counter
 class KNearestNeighbor(object):
@@ -10,7 +11,6 @@ class KNearestNeighbor(object):
     """
     Train the classifier. For k-nearest neighbors this is just 
     memorizing the training data.
-
     Inputs:
     - X: A numpy array of shape (num_train, D) containing the training data
       consisting of num_train samples each of dimension D.
@@ -23,14 +23,12 @@ class KNearestNeighbor(object):
   def predict(self, X, k=1, num_loops=0):
     """
     Predict labels for test data using this classifier.
-
     Inputs:
     - X: A numpy array of shape (num_test, D) containing test data consisting
          of num_test samples each of dimension D.
     - k: The number of nearest neighbors that vote for the predicted labels.
     - num_loops: Determines which implementation to use to compute distances
       between training points and testing points.
-
     Returns:
     - y: A numpy array of shape (num_test,) containing predicted labels for the
       test data, where y[i] is the predicted label for the test point X[i].  
@@ -51,10 +49,8 @@ class KNearestNeighbor(object):
     Compute the distance between each test point in X and each training point
     in self.X_train using a nested loop over both the training data and the 
     test data.
-
     Inputs:
     - X: A numpy array of shape (num_test, D) containing test data.
-
     Returns:
     - dists: A numpy array of shape (num_test, num_train) where dists[i, j]
       is the Euclidean distance between the ith test point and the jth training
@@ -82,7 +78,6 @@ class KNearestNeighbor(object):
     """
     Compute the distance between each test point in X and each training point
     in self.X_train using a single loop over the test data.
-
     Input / Output: Same as compute_distances_two_loops
     """
     num_test = X.shape[0]
@@ -105,7 +100,6 @@ class KNearestNeighbor(object):
     """
     Compute the distance between each test point in X and each training point
     in self.X_train using no explicit loops.
-
     Input / Output: Same as compute_distances_two_loops
     """
     num_test = X.shape[0]
@@ -139,11 +133,9 @@ class KNearestNeighbor(object):
     """
     Given a matrix of distances between test points and training points,
     predict a label for each test point.
-
     Inputs:
     - dists: A numpy array of shape (num_test, num_train) where dists[i, j]
       gives the distance betwen the ith test point and the jth training point.
-
     Returns:
     - y: A numpy array of shape (num_test,) containing predicted labels for the
       test data, where y[i] is the predicted label for the test point X[i].  
