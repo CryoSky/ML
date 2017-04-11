a203e77dbc304ada475f3374795f06b350087032
xun6000 <xun6000@gmail.com>
2017-04-11 14:59:25 -0500

    Add files via upload

--- hw6/anomaly_detection/utils_anomaly.py
+++ hw6/anomaly_detection/utils_anomaly.py
@@ -7,10 +7,13 @@ def estimate_gaussian(X):
     """
     mu = np.zeros((X.shape[1],))
     var = np.zeros((X.shape[1],))
+    
     ####################################################################
     #               YOUR CODE HERE                                     #
     ####################################################################
+    mu = X.mean(axis=0) 
     
+    var = X.var(axis=0)
     ####################################################################
     #               END YOUR CODE                                      #
     ####################################################################
@@ -23,17 +26,32 @@ def select_threshold(yval,pval):
     threshold to use for selecting outliers based on the results from a
     validation set (pval) and the ground truth (yval).
     """
-
+    print(pval)
     best_epsilon = 0
     bestF1 = 0
+   
     stepsize = (max(pval)-min(pval))/1000
-    for epsilon in np.arange(min(pval)+stepsize, max(pval), stepsize):
+    for epsilon in np.arange(min(pval), max(pval), stepsize):
         
         ####################################################################
         #                 YOUR CODE HERE                                   #
         ####################################################################
         
-        bestF1 = 0
+        
+        preds = pval < epsilon
+        
+        tp = np.sum(np.logical_and(preds == 1, yval == 1)).astype(float)
+        fp = np.sum(np.logical_and(preds == 1, yval == 0)).astype(float)
+        fn = np.sum(np.logical_and(preds == 0, yval == 1)).astype(float)
+
+        precision = tp / (tp + fp)
+        recall = tp / (tp + fn)
+        f1 = (2 * precision * recall) / (precision + recall)
+
+        if f1 > bestF1:
+            bestF1 = f1
+            best_epsilon = epsilon
+
 
         ####################################################################
         #                 END YOUR CODE                                    #
